dc92fb899056a0c3c06b8c8d00176fca46904a76
Brunno dos Santos Garcia <brunnogh2025@gmail.com>
2026-09-08 16:34:37 -0300

    Filtro de autenticação criado

--- digital/src/main/java/hospital/digital/configuration/TokenConfig.java
+++ digital/src/main/java/hospital/digital/configuration/TokenConfig.java
@@ -2,7 +2,10 @@ package hospital.digital.configuration;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import hospital.digital.entity.UserDetailsWithId;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
@@ -10,15 +13,17 @@ import java.time.Instant;
 
 @Component
 @ConfigurationProperties(prefix = "jwt")
+@Slf4j
 public class TokenConfig {
 
     String secret;
     Long expiration;
     String issuer;
 
+    Algorithm algorithm = Algorithm.HMAC256(secret);
+
     public String generateToken(UserDetailsWithId user){
 
-        Algorithm algorithm = Algorithm.HMAC256(secret);
 
         return JWT.create()
                 .withClaim("UserId", user.getId())
@@ -29,28 +34,25 @@ public class TokenConfig {
                 .sign(algorithm);
     }
 
-    public String getSecret() {
-        return secret;
+    public boolean isTokenValid(String jwt){
+        try {
+            DecodedJWT decodedJWT = JWT.require(algorithm)
+                    .build()
+                    .verify(jwt);
+            return true;
+        } catch (JWTVerificationException e) {
+            log.warn("Token inválido: {}", e.getMessage());
+            return false;
+        }
+
     }
 
-    public void setSecret(String secret) {
-        this.secret = secret;
+    public String getUsernameFromToken(String jwt){
+        return JWT.require(algorithm)
+                .build()
+                .verify(jwt)
+                .getSubject();
     }
 
-    public Long getExpiration() {
-        return expiration;
-    }
-
-    public void setExpiration(Long expiration) {
-        this.expiration = expiration;
-    }
-
-    public String getIssuer() {
-        return issuer;
-    }
-
-    public void setIssuer(String issuer) {
-        this.issuer = issuer;
-    }
 
 }
